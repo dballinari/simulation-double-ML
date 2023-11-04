b9787a5ced92c7277299f3532178240e1275f691
Daniele Ballinari <daniele.ballinari@gmail.com>
2023-11-04 19:57:17 +0100

    Define experiments*

--- experiments.bat
+++ experiments.bat
@@ -1,4 +1,6 @@
-python run_simulation.py --num_simulations 1000 --n 1000 --p 100 --n_estimators 200 --n_jobs -1 --cate_type complex
-python run_simulation.py --num_simulations 1000 --n 4000 --p 100 --n_estimators 200 --n_jobs -1 --cate_type complex
-python run_simulation.py --num_simulations 1000 --n 1000 --p 100 --n_estimators 200 --n_jobs -1 --cate_type sine
-python run_simulation.py --num_simulations 1000 --n 4000 --p 100 --n_estimators 200 --n_jobs -1 --cate_type sine
+python run_simulation.py --num_simulations 1000 --n 2000 --p 20 --n_estimators 200 --n_jobs -1 --cate_type sine
+python run_simulation.py --num_simulations 1000 --n 2000 --p 20 --n_estimators 200 --n_jobs -1 --cate_type complex
+python run_simulation.py --num_simulations 1000 --n 2000 --p 40 --n_estimators 200 --n_jobs -1 --cate_type sine
+python run_simulation.py --num_simulations 1000 --n 2000 --p 40 --n_estimators 200 --n_jobs -1 --cate_type complex
+python run_simulation.py --num_simulations 1000 --n 4000 --p 20 --n_estimators 200 --n_jobs -1 --cate_type sine
+python run_simulation.py --num_simulations 1000 --n 4000 --p 20 --n_estimators 200 --n_jobs -1 --cate_type complex
