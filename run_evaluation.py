ef84bd07586647d8ef473b9c5dd4ab01dceb7813
Daniele Ballinari <daniele.ballinari@gmail.com>
2023-11-05 10:18:45 +0100

    Add mean ATE to histogram

--- run_evaluation.py
+++ run_evaluation.py
@@ -7,10 +7,11 @@ def plot_estimator_distribution(estimator: np.ndarray, true_value: float, ax: pl
     std_estimator = np.nanstd(estimator)
     x = np.linspace(-5*std_estimator + mean_estimator, 5*std_estimator + mean_estimator, 100)
     pdf = 1/np.sqrt(2*np.pi*std_estimator)*np.exp(-((x-mean_estimator)/std_estimator)**2/2)
-    hist_values, bins, _ = ax.hist(estimator, bins=50, alpha=0.5, density=True)
+    hist_values, _, _ = ax.hist(estimator, bins=50, alpha=0.5, density=True)
     pdf *= np.max(hist_values)/np.max(pdf)
     ax.plot(x, pdf, 'r--')
-    ax.axvline(true_value, color='k', linestyle='dashed', linewidth=1)
+    ax.axvline(true_value, color='k', linewidth=1)
+    ax.vlines(mean_estimator, ymin=0, ymax=max(pdf), color='r', linestyle='dashed')
     ax.set_title(title)
 
 def evaluate_estimation(ate: np.ndarray, ate_true: float, ate_var: float, n_obs: int, level: float=0.95) -> dict:
@@ -23,15 +24,24 @@ def evaluate_estimation(ate: np.ndarray, ate_true: float, ate_var: float, n_obs:
         'std_estimate': np.nanstd(ate),
         'coverage': np.mean((ate_true > ate - quantile_normal*np.sqrt(ate_var/n_obs)) & (ate_true < ate + quantile_normal*np.sqrt(ate_var/n_obs))),
         'jaque_bera': scipy.stats.jarque_bera(ate)[0],
+        'jaque_bera_pvalue': scipy.stats.jarque_bera(ate)[1],
+        'kurtosis': scipy.stats.kurtosis(ate),
+        'skewness': scipy.stats.skew(ate)
     }
 
 
-results = np.load('results/ate_100_2000_20_0.5_2_4_1.0_sine_100_123.npz', allow_pickle=True)
+results = np.load('results/ate_1000_2000_20_0.5_2_4_1.0_sine_200_123.npz', allow_pickle=True)
 simulation_settings = results.f.simulation_settings.tolist()
 
-print(evaluate_estimation(results.f.ate, simulation_settings['true_ate'], results.f.ate_var, simulation_settings['n'], level=0.95))
-print(evaluate_estimation(results.f.ate_reg_split, simulation_settings['true_ate'], results.f.ate_reg_split_var, simulation_settings['n'], level=0.95))
-print(evaluate_estimation(results.f.ate_reg, simulation_settings['true_ate'], results.f.ate_reg_var, simulation_settings['n'], level=0.95))
+print('Double-ML:')
+for key, value in evaluate_estimation(results.f.ate, simulation_settings['true_ate'], results.f.ate_var, simulation_settings['n'], level=0.95).items():
+    print(f'\t{key}: {value}')
+print('\nRegression adjustment sample-splitting')
+for key, value in evaluate_estimation(results.f.ate_reg_split, simulation_settings['true_ate'], results.f.ate_reg_split_var, simulation_settings['n'], level=0.95).items():
+    print(f'\t{key}: {value}')
+print('\nRegression adjustment')
+for key, value in evaluate_estimation(results.f.ate_reg, simulation_settings['true_ate'], results.f.ate_reg_var, simulation_settings['n'], level=0.95).items():
+    print(f'\t{key}: {value}')
 
 fig, ax = plt.subplots(ncols=3)
 plot_estimator_distribution(results.f.ate, simulation_settings['true_ate'], ax[0], 'ATE')
