b986e1da7bdd0f490299a6439b7f57a23c11c4cb
Daniele Ballinari <daniele.ballinari@gmail.com>
2023-11-18 14:37:58 +0100

    Adjust default cate function

--- simulation/dgps.py
+++ simulation/dgps.py
@@ -28,9 +28,18 @@ def propensity_scores(x: np.ndarray, alpha: float, beta: int, gamma: int) -> np.
     ps = alpha*(1 + stats.beta.cdf(f, beta, gamma))
     return ps
 
-def sim_covariates(n: int, p: int) -> np.ndarray:
-    # simulate nxp covariates from a uniform distribution
-    x = np.random.uniform(size=(n, p))
+def sim_covariates(n: int, p: int, type: str='uniform') -> np.ndarray:
+    if type == 'uniform':
+        # simulate nxp covariates from a uniform distribution
+        x = np.random.uniform(size=(n, p))
+    elif type == 'normal':
+        # define covariance matrix where entries are 0.7^(|i-j|) for i,j=1,...,p
+        cov = np.zeros((p,p))
+        for i in range(p):
+            for j in range(p):
+                cov[i,j] = 0.7**(np.abs(i-j))
+        # simulate nxp covariates from a multivariate normal distribution with mean 0 and covariance matrix cov
+        x = np.random.multivariate_normal(np.zeros(p), cov, size=n)
     return x
 
 def outcomes_not_treated(x: np.ndarray) -> np.ndarray:
@@ -46,11 +55,11 @@ def _base_outcomes(x: np.ndarray) -> np.ndarray:
     return np.sin(np.prod(x[:,:2], axis=1)*np.pi) + 2*(x[:,3]-0.5)**2 + 0.5*x[:,4] 
 
 
-def _cate(type: str):
+def _cate(type: str, amplitude: int=3):
     # returns CATE with expected value equal to 1
     cates = {
         'complex': lambda x: (1 + (1/(1+np.exp(-20*(x[:,0]-0.5))) - 0.5))*(1 + (1/(1+np.exp(-20*(x[:,1]-0.5))) - 0.5)), # as in the paper https://arxiv.org/abs/1510.04342
-        'sine': lambda x: (np.cos(x[:,0]*np.pi) + np.sin(x[:,1]*np.pi))*np.pi/2, # in expectation equal to 2/pi
+        'sine': lambda x: (np.cos(amplitude*x[:,0]*np.pi) + np.sin(amplitude*x[:,1]*np.pi))*amplitude*np.pi/2, # in expectation equal to 2/(amplitude*pi)
         'constant': lambda x: 1,
     }
     if type not in cates.keys():
