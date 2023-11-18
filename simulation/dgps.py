72a82088d7cbb24ad52181ffed9447f3f8f49d20
Daniele Ballinari <daniele.ballinari@gmail.com>
2023-11-18 12:15:36 +0100

    Fix bug and improve performance

--- simulation/dgps.py
+++ simulation/dgps.py
@@ -18,7 +18,7 @@ def sim_outcomes(n: int, p: int, alpha: float, beta: int, gamma: int, true_ate: 
     y0 = outcomes_not_treated(x)
     y1 = outcomes_treated(x, true_ate, cate_type)
     # observed outcomes
-    y = y0*(1-w) + y1*w
+    y = y0*(1-w) + y1*w + np.random.normal(size=x.shape[0])
     return x, w, y
 
 def propensity_scores(x: np.ndarray, alpha: float, beta: int, gamma: int) -> np.ndarray:
@@ -34,23 +34,24 @@ def sim_covariates(n: int, p: int) -> np.ndarray:
     return x
 
 def outcomes_not_treated(x: np.ndarray) -> np.ndarray:
-    mu = _base_outcomes(x) + np.random.normal(size=x.shape[0])
+    mu = _base_outcomes(x)
     return mu
 
 def outcomes_treated(x: np.ndarray, true_ate: float, cate_type: str='constant') -> np.ndarray:
-    tau = _cate(x, cate_type)
-    mu = true_ate*tau + _base_outcomes(x) + np.random.normal(size=x.shape[0])
+    tau = _cate(cate_type)(x)
+    mu = true_ate*tau + _base_outcomes(x)
     return mu
 
 def _base_outcomes(x: np.ndarray) -> np.ndarray:
     return np.sin(np.prod(x[:,:2], axis=1)*np.pi) + 2*(x[:,3]-0.5)**2 + 0.5*x[:,4] 
 
-def _cate(x: np.ndarray, type: str) -> np.ndarray:
+
+def _cate(type: str):
     # returns CATE with expected value equal to 1
     cates = {
-        'complex': (1 + (1/(1+np.exp(-20*(x[:,0]-0.5))) - 0.5))*(1 + (1/(1+np.exp(-20*(x[:,1]-0.5))) - 0.5)), # as in the paper https://arxiv.org/abs/1510.04342
-        'sine': (np.cos(x[:,0]*np.pi) + np.sin(x[:,1]*np.pi))*np.pi/2, # in expectation equal to 2/pi
-        'constant': 1,
+        'complex': lambda x: (1 + (1/(1+np.exp(-20*(x[:,0]-0.5))) - 0.5))*(1 + (1/(1+np.exp(-20*(x[:,1]-0.5))) - 0.5)), # as in the paper https://arxiv.org/abs/1510.04342
+        'sine': lambda x: (np.cos(x[:,0]*np.pi) + np.sin(x[:,1]*np.pi))*np.pi/2, # in expectation equal to 2/pi
+        'constant': lambda x: 1,
     }
     if type not in cates.keys():
         raise ValueError(f"CATE type {type} not recognized")
