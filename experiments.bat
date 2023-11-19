a1a27ec73db2c32d225102d270636ba3553fef48
Daniele Ballinari <daniele.ballinari@gmail.com>
2023-11-19 16:45:09 +0100

    Define new experiments

--- experiments.bat
+++ experiments.bat
@@ -1,2 +1,3 @@
-python run_simulation.py --num_simulations 1000 --n 2000 --p 20 --n_estimators 200 --n_jobs -1 --cate_type complex --n_folds 2
-python run_simulation.py --num_simulations 1000 --n 2000 --p 20 --n_estimators 200 --n_jobs -1 --cate_type sine --n_folds 2
+python run_simulation.py --num_simulations 1000 --n 2000 --p 20 --n_estimators 200 --n_jobs -1 --mode 1 --n_folds 2
+python run_simulation.py --num_simulations 1000 --n 2000 --p 20 --n_estimators 200 --n_jobs -1 --mode 2 --n_folds 2
+python run_simulation.py --num_simulations 1000 --n 2000 --p 20 --n_estimators 200 --n_jobs -1 --mode 3 --n_folds 2
